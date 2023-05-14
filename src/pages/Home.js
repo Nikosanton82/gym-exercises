ce1fc0355b22a3afe77ce14776c814c8cba8725c
Nikos <nikosant82@gmail.com>
2023-05-14 18:20:40 +0200

    Fetch Data & Horizontal Scrollbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
 
-import HeroBanner from "./HeroBanner";
-import SearchExercises from "./SearchExercises";
-import Exercises from "./Exercises";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
+import Exercises from "../components/Exercises";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
   );
 };
